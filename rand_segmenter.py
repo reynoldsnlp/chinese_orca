e7c3bd999c6c21b392c91f44289eb4e89d35a87b
nlpcall <reynoldsnlp@users.noreply.github.com>
2018-02-09 08:45:35 -0700

    add parametrization to rand_segmenter.py

--- rand_segmenter.py
+++ rand_segmenter.py
@@ -1,17 +1,25 @@
-"""Parse spaceless text randomly."""
+"""Parse spaceless text randomly.
 
+Test by using...
+$ python3 -c "print('c'*10000)" | python3 rand_segmenter.py
+"""
+
+from collections import Counter
+from pprint import pprint
 from random import random
 from statistics import mean
 from sys import stdin
+from sys import stderr
 
 MAX_WORD_LEN = 5
+PROB = 0.46  # probability that word with continue without a space
 
 in_text = (c for c in stdin.read())  # generator object
 
 out_text = next(in_text)  # start with character (not a space)
 word_len = 1
 while True:
-    if random() > 0.55:
+    if random() <= PROB:  
         try:
             out_text += next(in_text)
             word_len += 1
@@ -20,13 +28,19 @@ while True:
         if word_len >= MAX_WORD_LEN:
             try:
                 out_text += ' ' + next(in_text)
+                word_len = 1
             except StopIteration:  # means the generator is empty
                 break
     else:
         try:
             out_text += ' ' + next(in_text)
+            word_len = 1
         except StopIteration:  # means the generator is empty
             break
 
-print(out_text)
-print('mean word length:', mean([len(w) for w in out_text.split()])) 
+print(out_text, end='')
+w_lens = [len(w) for w in out_text.split()]
+print('mean word length:', mean(w_lens), file=stderr) 
+print('word length distribution:', file=stderr)
+for k, v in Counter(w_lens).items():
+    print('\t', k, ': ', v, sep='', file=stderr)
